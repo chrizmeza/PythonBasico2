4df71f71b5ff156730fc4a6a6cb61086a84a4790
chrizmeza <angelariasmezacrack@gmail.com>
2026-02-12 19:31:02 -0800

    descripcion del cambio README.md

---
+++ sesion_01.py
@@ -0,0 +1,48 @@
+#numerico y flotantes
+
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(int(1+2))
+print(int(10*2))
+print("===== operadores matematicos =====")
+# +
+# -
+# *
+# /
+# ""
+#  % MODULO
+
+
+print(float(10%2))
+print(float(25%4))
+
+
+
+
+ventas = 1818535602
+print("nuestras ventas fueron", ventas)
+
+
+is_active = True
+print(bool(is_active))
+
+
+game_over = False
+print(game_over)
+
+
+edad = 16
+if(edad >= 18):
+    print("si puedes entar al bar")
+else:
+    print("no puedes entar al bar")
+   
+mi_numero = int(input("¿cual es el numero que deseas verificar?:"))
+print("el numero que deseas verificar {el_numero}")
+if mi_numero % 2 == 0:
+    print(f"el numero{mi_numero}es par¡¡¡")
+else:
+    print(f"el numero {mi_numero}es impar")
+
+   
